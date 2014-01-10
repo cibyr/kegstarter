66320ae7fca291e1120c83a15f83ffa87960d7f8
Justin Derby <justderb@gmail.com>
2014-01-09 23:07:04 -0800

    Adding Keg Master table to admin panel until there is a process
    in place to grant Keg Master status.  We will do it
    manual for now.

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Brewery, Keg
+from .models import Brewery, Keg, KegMaster
 
 admin.site.register(Brewery)
 admin.site.register(Keg)
+admin.site.register(KegMaster)
