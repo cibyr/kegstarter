588f4bb149c3d66a791a5682ac1a5480c167ecbb
Justin Derby <justderb@gmail.com>
2014-01-20 21:45:55 -0800

    No votes will be set to 0 votes - #19

--- main/context_processors/user_stats.py
+++ main/context_processors/user_stats.py
@@ -5,6 +5,9 @@ def get_user_votes(request):
     # Must be logged in to have a balance
     if request.user.id:
         votes = get_user_balance(request.user)
+        # No votes? Set to zero
+        if not votes:
+            votes = 0
     else:
         votes = None
 
