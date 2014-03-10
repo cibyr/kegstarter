aad7ffe25ff0976de5fad42c9d7e668d4ea06f34
Ian Cullinan <cibyr@cibyr.com>
2014-03-09 21:29:34 -0700

    Show keg suggestions in admin

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Brewery, Keg, KegMaster, UntappdCredentials
+from .models import Brewery, Keg, KegMaster, Suggestion, UntappdCredentials
 
 admin.site.register(Brewery)
 admin.site.register(Keg)
 admin.site.register(KegMaster)
+admin.site.register(Suggestion)
 
 # Just in case we need to reset them (there is no identifying info in this)
 admin.site.register(UntappdCredentials)
