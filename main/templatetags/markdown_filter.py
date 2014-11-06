8bda192c83f8a374eae30c96a3d64e8c423ff789
Ian Cullinan <cibyr@cibyr.com>
2014-11-05 21:02:33 -0800

    Support markdown in comments

--- main/templatetags/markdown_filter.py
+++ main/templatetags/markdown_filter.py
@@ -1,3 +1,4 @@
+import bleach
 import markdown
 
 from django import template
@@ -7,8 +8,25 @@ from django.utils.safestring import mark_safe
 
 register = template.Library()
 
+ALLOWED_TAGS = [
+    'a', 'abbr', 'acronym', 'b', 'blockquote', 'br', 'code', 'col', 'colgroup',
+    'del', 'em', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'img', 'li',
+    'ol', 'p', 'pre', 'strike', 'strong', 'sub', 'ul', 'table', 'tbody', 'td',
+    'tfoot', 'th', 'thead', 'tr',
+]
+
+ALLOWED_ATTRIBUTES = {
+    'a': ['href', 'title'],
+    'abbr': ['title'],
+    'acronym': ['title'],
+    'img': ['src', 'alt']
+}
+
 @register.filter(is_safe=True)
 @stringfilter
 def format_markdown(value):
-    return mark_safe(markdown.markdown(force_unicode(value), safe_mode=True,
-        enable_attributes=False))
+    return mark_safe(bleach.linkify(bleach.clean(markdown.markdown(force_unicode(value),
+                                                                   extensions=['markdown.extensions.sane_lists'],
+                                                                   output_format='html5',
+                                                                   lazy_ol=False),
+                                                 tags=ALLOWED_TAGS, attributes=ALLOWED_ATTRIBUTES)))
